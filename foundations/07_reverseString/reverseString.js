696f90524f3cb9cf744e83864829a09eccddaf1a
Adhil Khan <adhilkhan2023@gmail.com>
2026-07-11 10:34:26 +0530

    Complete '07_reverseString' exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let charactersOfWord = word.split("");
+  charactersOfWord.reverse();
 
+  word = charactersOfWord.join("");
+  return word;
 };
 
 // Do not edit below this line
