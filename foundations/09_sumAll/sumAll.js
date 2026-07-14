0c690c194fbbd1cff6103c90f512d9a116b2501f
Adhil Khan <adhilkhan2023@gmail.com>
2026-07-14 07:22:38 +0530

    Complete '09_sumAll' exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startingNumber, endingNumber) {
+  if((startingNumber < 0 || startingNumber.toString().split("").includes(".") || 
+    typeof(startingNumber) === "string" || typeof(startingNumber) === "object") 
+    || (endingNumber < 0 || endingNumber.toString().split("").includes(".") || 
+    typeof(endingNumber) === "string") || typeof(endingNumber) === "object"){
+      return "ERROR";
+    }else if(startingNumber > endingNumber){
+      let sum = 0;
 
+      for(let i = startingNumber; i >= endingNumber; i--){
+        sum += i;
+      }
+
+      return sum;
+    }else{
+      let sum = 0;
+
+      for(let i = startingNumber; i <= endingNumber; i++){
+        sum += i;
+      }
+
+      return sum;
+    }
 };
 
 // Do not edit below this line
